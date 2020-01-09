b64b0608204622beb93caa3b5bf31ff919546421
Pedro Luiz Alves de Oliveira <pedro.oliveira@smagalhaes.org>
2020-01-09 15:24:41 -0300

    Criação de times e adição e exclusão de membros de times

--- src/controllers/TeamController.js
+++ src/controllers/TeamController.js
@@ -5,7 +5,10 @@ module.exports = {
     async store(req, res) {
         try {
             const {player_id, name} = req.body;
-            
+            if (await Team.findOne({where: {player_id: player_id, name: name}})) return res.json({error: 'Usuário já tem um time com este nome'});
+            const team = Team.Create({player_id, name});
+            const team_members = TeamMembers.Create({player_id, team_id: team.id, in_admin: true});
+            return res.json({team, team_members});
         } catch(err) {
             res.json({error: err.message});
         }
@@ -13,7 +16,12 @@ module.exports = {
 
     async addMember(req, res) {
         try {
-
+            const {player_id, admin_id, team_id, player_in_admin} = req.body;
+            if (player_id == admin_id) return res.json({error: 'Administrador e usuário informado é o mesmo'});
+            if (!await TeamMembers.findOne({where: {player_id: admin_id, team_id: team_id, in_admin: true}})) return res.json({error: 'Usuário não é administrador.'});
+            if (await TeamMembers.findOne({where: {player_id: player_id, team_id: team_id}})) return res.json({error: 'Usuário já esta no time'});
+            const team_member = TeamMembers.Create({player_id: player_id, team_id: team_id, in_admin: player_in_admin});
+            return res.json(team_member);
         } catch(err) {
             res.json({error: err.message});
         }
@@ -21,7 +29,12 @@ module.exports = {
 
     async kickMember(req, res) {
         try {
-
+            const {player_id, admin_id, team_id} = req.body;
+            if (!await TeamMembers.findOne({where: {player_id: admin_id, team_id: team_id, in_admin: true}}) && player_id != admin_id) return res.json({error: 'Usuário não é administrador.'});
+            if (!await TeamMembers.findOne({where: {player_id: player_id, team_id: team_id}})) return res.json({error: 'Usuário não esta no time'});            
+            TeamMembers.delete({where: {player_id: player_id}});
+            return res.json({ok: true});
+            
         } catch(err) {
             res.json({error: err.message});
         }
